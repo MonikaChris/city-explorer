1a23800983d5cb21fc3a17e752057ee617762bc8
Monika <monidavies@gmail.com>
2022-09-05 10:06:36 -0700

    Adds callbacks to SearchBar

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -8,120 +8,118 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import axios from 'axios';
 
+
 class SearchBar extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             searchedCity: '',
-            currentLocationObj: {},
-            currentMap: '',
-            currentWeather: [],
-            currentMovies: {},
-            currentLocationError: '',
-            currentWeatherError: '',
-            currentMoviesError: '',
-            currentYelp: [],
-            currentYelpError: ''
+            locationObj: {},
+            map: '',
+            weather: [],
+            movies: {},
+            locationError: '',
+            weatherError: '',
+            moviesError: '',
+            yelp: [],
+            yelpError: ''
         }
     }
 
     getLocation = async (e) => {
         e.preventDefault();
         try {
-          const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_KEY}&q=${this.state.searchedCity}&format=json`;
-          const response = await axios.get(url);
-          this.setState({ currentLocationObj: response.data[0], currentLocationError: "" }, () => this.getWeather());
-          this.getMap(response.data[0].lat, response.data[0].lon);
-          this.getYelp(response.data[0].lat, response.data[0].lon);
-        } catch (err) {
-          this.setState( {currentLocationObj: {}, currentLocationErrorMessage: err.message, currentMap: '' })
-        }
-
-        this.getMovies(this.state.searchedCity);
-        
-    }
-    
-      getMap = async (lat, lon) => {
-        const url = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_KEY}&center=${lat},${lon}&zoom=12`;
-        const response = await axios.get(url);
-        this.setState({ currentMap: response.config.url });
-      }
-
-      getWeather = async () => {
-        try {
-            const url = `${process.env.REACT_APP_SERVER}/weather?lat=${this.state.currentLocationObj.lat}&lon=${this.state.currentLocationObj.lon}`;
+            const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_KEY}&q=${this.state.searchedCity}&format=json`;
             const response = await axios.get(url);
-            this.setState({ currentWeather: response.data, currentWeatherError: ''});
-          }
-          catch (err) {
-            this.setState( { currentWeather: [], currentWeatherError: err.message });
-          }
-      }
+            this.setState({ locationObj: response.data[0], locationError: "" }, () => this.getMap());
+        } catch (err) {
+            this.setState({ locationObj: {}, locationErrorMessage: err.message, map: '' })
+        }
+    }
 
-      getMovies = async (city) => {
+    getMap = async () => {
+        const url = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_KEY}&center=${this.state.locationObj.lat},${this.state.locationObj.lon}&zoom=12`;
+        const response = await axios.get(url);
+        this.setState({ map: response.config.url }, () => this.getWeather());
+    }
+
+    getWeather = async () => {
         try {
-            const url = `${process.env.REACT_APP_SERVER}/movies?searchQuery=${city}`;
+            const url = `${process.env.REACT_APP_SERVER}/weather?lat=${this.state.locationObj.lat}&lon=${this.state.locationObj.lon}`;
+            const response = await axios.get(url);
+            this.setState({ weather: response.data, weatherError: '' }, () => this.getYelp());
+        }
+        catch (err) {
+            this.setState({ weather: [], weatherError: err.message });
+        }
+    }
+
+    getYelp = async () => {
+        try {
+            const url = `${process.env.REACT_APP_SERVER}/yelp?lat=${this.state.locationObj.lat}&lon=${this.state.locationObj.lon}`;
+            const response = await axios.get(url);
+            console.log(response.data);
+            this.setState({ yelp: response.data }, () => this.getMovies());
+        } catch (err) {
+            this.setState({ yelp: [], yelpError: err.message });
+        }
+    }
+
+    getMovies = async () => {
+        try {
+            const url = `${process.env.REACT_APP_SERVER}/movies?searchQuery=${this.state.searchedCity}`;
             const response = await axios.get(url);
             //If not from cache, convert to object with timestamp of now
-            if(Array.isArray(response.data)) {
-                let movieObj = { data: response.data, timestamp: Date.now()};
-                this.setState( {currentMovies: movieObj, currentMoviesError: '' });
-            } else this.setState( {currentMovies: response.data, currentMoviesError: '' });
+            if (Array.isArray(response.data)) {
+                let movieObj = { data: response.data, timestamp: Date.now() };
+                this.setState({ movies: movieObj, moviesError: '' });
+            } else this.setState({ movies: response.data, moviesError: '' });
         } catch (err) {
-            this.setState( { currentMovies: {}, currentMoviesError: err.message });
+            this.setState({ movies: {}, moviesError: err.message });
         }
-      }
-
-      getYelp = async (lat, lon) => {
-        try {
-          const url = `${process.env.REACT_APP_SERVER}/yelp?lat=${lat}&lon=${lon}`;
-          const response = await axios.get(url);
-          console.log(response.data);
-          this.setState({currentYelp: response.data});
-        } catch (err) {
-          this.setState( {currentYelp: [], currentYelpError: err.message});
-        }
-
-      }
+    }
 
     render() {
         return (
-            <Container className="App">
-                <Container id="searchBar">
-                    <Form>
-                        <Row>
-                            <Col xs={6} sm={7} md={8} lg={10}>
-                                <Form.Control
-                                    type="text"
-                                    placeholder="Enter a City"
-                                    onChange={(event) => this.setState({ searchedCity: event.target.value })}
-                                />
-                            </Col>
-                            <Col xs={1} s={1} md={1} lg={1}>
-                                <Button
-                                    onClick={this.getLocation}
-                                    as="input"
-                                    type="submit"
-                                    variant="info"
-                                    value="Explore!"
-                                />
-                            </Col>
-                        </Row>
-                    </Form>
+            <>
+                <Container className="App">
+                    <Container id="searchBar">
+                        <Form>
+                            <Row>
+                                <Col xs={6} sm={7} md={8} lg={10}>
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="Enter a City"
+                                        onChange={(event) => this.setState({ searchedCity: event.target.value })}
+                                    />
+                                </Col>
+                                <Col xs={1} s={1} md={1} lg={1}>
+                                    <Button
+                                        onClick={this.getLocation}
+                                        as="input"
+                                        type="submit"
+                                        variant="info"
+                                        value="Explore!"
+                                    />
+                                </Col>
+                            </Row>
+                        </Form>
+                    </Container>
+                    <Display
+                        locationObj={this.state.locationObj}
+                        weather={this.state.weather}
+                        locationError={this.state.locationError}
+                        weatherError={this.state.weatherError}
+                        map={this.state.map}
+                        movies={this.state.movies}
+                        moviesError={this.state.moviesError}
+                        yelp={this.state.yelp}
+                        yelpError={this.state.yelpError}
+                        searchedCity={this.state.searchedCity}
+                    />
                 </Container>
-                <Display 
-                locationObj={this.state.currentLocationObj}
-                weather={this.state.currentWeather}
-                locationError={this.state.currentLocationError}
-                weatherError={this.state.currentWeatherError}
-                map={this.state.currentMap}
-                movies={this.state.currentMovies}
-                moviesError={this.state.currentMoviesError}
-                yelp={this.state.currentYelp}
-                yelpError={this.state.currentYelpError}
-                searchedCity={this.state.searchedCity}
-                />
-            </Container>
+                
+            </>
         )
     }
 }
