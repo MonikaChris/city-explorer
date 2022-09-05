27e9b4a2a6707b6fd69556d357e4989df0a369a1
Monika <monidavies@gmail.com>
2022-09-05 12:35:02 -0700

    Add food type, format restaurants

--- src/Yelp.js
+++ src/Yelp.js
@@ -8,39 +8,49 @@ class Yelp extends React.Component {
   render() {
     return (
       <>
-        
+
         {this.props.yelp.length > 0 &&
-        <Container>
+          <Container>
             <Card className="section-heading">
-            <Card.Title>Restaurants in the city of {this.props.searchedCity}</Card.Title>
+              <Card.Title>Restaurants in the city of {this.props.searchedCity}</Card.Title>
             </Card>
-          <Accordion defaultActiveKey="0">
-            
-            {this.props.yelp.map((restaurant, idx) => (
+            <Accordion defaultActiveKey="0">
+
+              {this.props.yelp.map((restaurant, idx) => (
                 <Restaurant
                   restaurant={restaurant}
                   idx={idx}
                 />
               ))
-            }
-          </Accordion>
-        </Container>
+              }
+            </Accordion>
+          </Container>
         }
-    </>
+      </>
     )
   }
 }
 
 class Restaurant extends React.Component {
   render() {
+    console.log(this.props.restaurant);
     return (
       <Accordion.Item eventKey={this.props.idx} key={this.props.idx}>
-        <Accordion.Header>{this.props.restaurant.name}</Accordion.Header>
+        <Accordion.Header>
+          <b>{this.props.restaurant.name}</b>
+          &emsp;&emsp;&emsp;
+          Cuisine: {this.props.restaurant.type}
+          &emsp;
+          Rating: {this.props.restaurant.rating}
+          &emsp;
+          Price: {this.props.restaurant.price}
+        </Accordion.Header>
         <Accordion.Body>
           <Card>
-            <Card.Text>Website: {this.props.restaurant.url}</Card.Text>
+            <a href={this.props.restaurant.url} target="_blank" rel="noopener noreferrer">{this.props.restaurant.name} Website</a>
+            <Card.Text>{this.props.restaurant.type}</Card.Text>
             <Card.Text>Price: {this.props.restaurant.price} Rating: {this.props.restaurant.rating}</Card.Text>
-            <Card.Img src={this.props.restaurant.image_url} />
+            <Card.Img id="yelpImages" src={this.props.restaurant.image_url} />
           </Card>
         </Accordion.Body>
       </Accordion.Item>
